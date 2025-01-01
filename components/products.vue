ac62868fa1a4f0252bdc3c387d9ad694685c321d
AMRWERY <amrmounir2@gmail.com>
2025-01-01 11:51:33 +0200

    .

--- components/products.vue
+++ components/products.vue
@@ -3,7 +3,7 @@
     <div class="mx-auto max-w-7xl">
       <div class="max-w-2xl py-8 mx-auto lg:max-w-none">
         <div class="space-y-12 lg:grid lg:grid-cols-3 lg:gap-x-6 lg:space-y-0">
-          <div v-for="card in store.products" :key="card" class="relative group">
+          <div v-for="card in paginatedProducts" :key="card.id" class="relative group">
             <img :src="card.imgOne"
               class="w-full rounded-lg bg-white object-cover group-hover:opacity-75 max-sm:h-80 sm:aspect-[2/1] lg:aspect-square" />
             <h3 class="mt-6 text-lg font-semibold text-gray-700">
@@ -21,7 +21,7 @@
       </div>
 
       <!-- pagination component -->
-      <pagination />
+      <pagination :total-pages="totalPages" :current-page="currentPage" @page-changed="onPageChanged" />
     </div>
   </div>
 </template>
@@ -32,4 +32,25 @@ const store = useNewProductsStoreStore()
 onMounted(() => {
   store.fetchProducts();
 });
+
+const currentPage = ref(1);
+const perPage = 10;
+
+const filteredProducts = computed(() => {
+  return store.products;
+});
+
+const totalPages = computed(() => {
+  return Math.ceil(filteredProducts.value.length / perPage);
+});
+
+const paginatedProducts = computed(() => {
+  const start = (currentPage.value - 1) * perPage;
+  const end = start + perPage;
+  return filteredProducts.value.slice(start, end);
+});
+
+const onPageChanged = (pageNumber) => {
+  currentPage.value = pageNumber;
+};
 </script>
