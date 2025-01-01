9b9c6c2c2b378a8ec3ce6a9ddbee6bc6baf58e5c
AMRWERY <amrmounir2@gmail.com>
2025-01-01 17:37:48 +0200

    ...

--- pages/login.vue
+++ pages/login.vue
@@ -1,35 +1,43 @@
 <template>
   <div>
-    <div class="bg-fixed bg-center bg-cover" style="background-image: url('https://picsum.photos/1920/1080')">
-      <div class="flex items-center justify-center h-screen">
-        <div class="p-8 mx-4 bg-white rounded shadow-md md:w-1/2 lg:w-1/3">
-          <h1 class="mb-8 text-3xl font-bold text-center">Login</h1>
-          <ClientOnly>
-            <dynamic-inputs v-model="data.email" :label="t('form.email')" :placeholder="t('form.enter_your_email')"
-              type="email" :validation="('required|email|ends_with:lugar.com')" :required="true" />
-            <dynamic-inputs v-model="data.password" :label="t('form.password')"
-              :placeholder="t('form.enter_your_password')" type="password" :validation="'required|password|length:6,7'"
-              :required="true" />
+    <div class="max-w-screen-xl px-4 py-16 mx-auto sm:px-6 lg:px-8">
+      <div class="max-w-md mx-auto">
+        <h1 class="text-2xl font-bold text-center text-black sm:text-3xl">
+          Get started today
+        </h1>
+        <ClientOnly>
+          <div class="p-4 mb-6 space-y-4 rounded-lg shadow-lg sm:p-6 lg:p-8">
+            <p class="text-lg font-medium text-center">Sign in to your account</p>
+            <div>
+              <dynamic-inputs v-model="data.email" :label="t('form.email')" :placeholder="t('form.enter_your_email')"
+                type="email" :validation="('required|email|ends_with:lugar.com')" :required="true" />
+            </div>
+            <div>
+              <dynamic-inputs v-model="data.password" :label="t('form.password')"
+                :placeholder="t('form.enter_your_password')" type="password"
+                :validation="'required|password|length:6,7'" :required="true" />
+            </div>
+
             <div class="mt-6">
               <button type="submit" :disabled="loading" @click="signIn"
-                class="w-[400px] px-4 py-2 font-bold text-white bg-blue-500 rounded hover:bg-blue-700 focus:outline-none focus:shadow-outline">
+                class="block w-full px-4 py-2 font-bold text-white bg-black border border-black rounded hover:bg-white hover:text-black focus:outline focus:shadow-outline">
                 <div class="flex items-center justify-center" v-if="loading">
                   <span class="text-center me-2">{{ $t('loading_btn.logging') }}...</span>
                   <icon name="svg-spinners:270-ring-with-bg" />
                 </div>
                 <span v-else>{{ $t('form.login') }}</span>
               </button>
-            </div>
-          </ClientOnly>
 
-          <div v-if="errorMessage" class="mt-2 text-sm text-red-500">
-            {{ errorMessage }}
+            </div>
+            <div v-if="errorMessage" class="mt-2 text-sm text-center text-red-500">
+              {{ errorMessage }}
+            </div>
           </div>
-        </div>
+        </ClientOnly>
       </div>
     </div>
 
-    <!-- dynamic-toast component -->
+    <!-- dynamic-toast component  -->
     <div class="fixed z-50 pointer-events-none bottom-5 start-5 w-96">
       <div class="pointer-events-auto">
         <dynamic-toast v-if="showToast" :title="toastTitle" :message="toastMessage" :toastType="toastType"
