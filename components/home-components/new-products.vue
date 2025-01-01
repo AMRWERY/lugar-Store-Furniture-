2d44ae1ac2dd58592ae32d340033135d9bedb29d
AMRWERY <amrmounir2@gmail.com>
2025-01-01 21:47:51 +0200

    .

--- components/home-components/new-products.vue
+++ components/home-components/new-products.vue
@@ -12,22 +12,34 @@
         <ul class="flex flex-row flex-wrap mx-auto mb-5 list-none border-b-0 ps-0 space-s-4" role="tablist"
           data-twe-nav-ref>
           <li role="presentation" class="mx-1">
-            <a href="#tabs-home"
+            <a href="#tabs-sofa"
               class="my-2 block rounded-full border-2 border-gray-300 px-7 pb-3.5 pt-4 text-sm leading-tight text-neutral-600 font-semibold hover:isolate hover:border-gray-600 hover:bg-neutral-100 focus:outline-none focus:border-gray-800 data-[twe-nav-active]:border-gray-800 data-[twe-nav-active]:text-gray-800 capitalize"
-              data-twe-toggle="pill" data-twe-target="#tabs-home" data-twe-nav-active role="tab"
-              aria-controls="tabs-home" aria-selected="true">Home</a>
+              data-twe-toggle="pill" data-twe-target="#tabs-sofa" data-twe-nav-active role="tab"
+              aria-controls="tabs-sofa" aria-selected="true">Sofa</a>
           </li>
           <li role="presentation" class="mx-1">
-            <a href="#tabs-profile"
+            <a href="#tabs-chairs"
               class="my-2 block rounded-full border-2 border-gray-300 px-7 pb-3.5 pt-4 text-sm leading-tight text-neutral-600 font-semibold hover:isolate hover:border-gray-600 hover:bg-neutral-100 focus:outline-none focus:border-gray-800 data-[twe-nav-active]:border-gray-800 data-[twe-nav-active]:text-gray-800 capitalize"
-              data-twe-toggle="pill" data-twe-target="#tabs-profile" role="tab" aria-controls="tabs-profile"
-              aria-selected="false">Profile</a>
+              data-twe-toggle="pill" data-twe-target="#tabs-chairs" role="tab" aria-controls="tabs-chairs"
+              aria-selected="false">Chairs</a>
           </li>
           <li role="presentation" class="mx-1">
-            <a href="#tabs-messages"
+            <a href="#tabs-bed-rooms"
               class="my-2 block rounded-full border-2 border-gray-300 px-7 pb-3.5 pt-4 text-sm leading-tight text-neutral-600 font-semibold hover:isolate hover:border-gray-600 hover:bg-neutral-100 focus:outline-none focus:border-gray-800 data-[twe-nav-active]:border-gray-800 data-[twe-nav-active]:text-gray-800 capitalize"
-              data-twe-toggle="pill" data-twe-target="#tabs-messages" role="tab" aria-controls="tabs-messages"
-              aria-selected="false">Messages</a>
+              data-twe-toggle="pill" data-twe-target="#tabs-bed-rooms" role="tab" aria-controls="tabs-bed-rooms"
+              aria-selected="false">Bed Rooms</a>
+          </li>
+          <li role="presentation" class="mx-1">
+            <a href="#tabs-living-room"
+              class="my-2 block rounded-full border-2 border-gray-300 px-7 pb-3.5 pt-4 text-sm leading-tight text-neutral-600 font-semibold hover:isolate hover:border-gray-600 hover:bg-neutral-100 focus:outline-none focus:border-gray-800 data-[twe-nav-active]:border-gray-800 data-[twe-nav-active]:text-gray-800 capitalize"
+              data-twe-toggle="pill" data-twe-target="#tabs-living-room" role="tab" aria-controls="tabs-living-room"
+              aria-selected="false">Living Room</a>
+          </li>
+          <li role="presentation" class="mx-1">
+            <a href="#tabs-dining-room"
+              class="my-2 block rounded-full border-2 border-gray-300 px-7 pb-3.5 pt-4 text-sm leading-tight text-neutral-600 font-semibold hover:isolate hover:border-gray-600 hover:bg-neutral-100 focus:outline-none focus:border-gray-800 data-[twe-nav-active]:border-gray-800 data-[twe-nav-active]:text-gray-800 capitalize"
+              data-twe-toggle="pill" data-twe-target="#tabs-dining-room" role="tab" aria-controls="tabs-dining-room"
+              aria-selected="false">Dining Room</a>
           </li>
         </ul>
       </div>
@@ -35,16 +47,24 @@
       <!-- Tabs content -->
       <div class="mb-6">
         <div class="hidden opacity-100 transition-opacity duration-150 ease-linear data-[twe-tab-active]:block"
-          id="tabs-home" role="tabpanel" aria-labelledby="tabs-home-tab" data-twe-tab-active>
-          Home tab content
+          id="tabs-sofa" role="tabpanel" aria-labelledby="tabs-sofa-tab" data-twe-tab-active>
+          Sofa tab content
         </div>
         <div class="hidden opacity-0 transition-opacity duration-150 ease-linear data-[twe-tab-active]:block"
-          id="tabs-profile" role="tabpanel" aria-labelledby="tabs-profile-tab">
-          Profile tab content
+          id="tabs-chairs" role="tabpanel" aria-labelledby="tabs-chairs-tab">
+          Chairs tab content
         </div>
         <div class="hidden opacity-0 transition-opacity duration-150 ease-linear data-[twe-tab-active]:block"
-          id="tabs-messages" role="tabpanel" aria-labelledby="tabs-profile-tab">
-          Messages tab content
+          id="tabs-bed-rooms" role="tabpanel" aria-labelledby="tabs-bed-rooms-tab">
+          Bed Rooms tab content
+        </div>
+        <div class="hidden opacity-0 transition-opacity duration-150 ease-linear data-[twe-tab-active]:block"
+          id="tabs-living-room" role="tabpanel" aria-labelledby="tabs-living-room-tab">
+          Living Room tab content
+        </div>
+        <div class="hidden opacity-0 transition-opacity duration-150 ease-linear data-[twe-tab-active]:block"
+          id="tabs-dining-room" role="tabpanel" aria-labelledby="tabs-dining-room-tab">
+          Dining Room tab content
         </div>
       </div>
     </div>
