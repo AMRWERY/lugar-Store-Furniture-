99edaf9b2d51771883a8e5826e95a1450ca3d339
AMRWERY <amrmounir2@gmail.com>
2025-01-01 20:55:09 +0200

    .

--- components/home-components/new-products.vue
+++ components/home-components/new-products.vue
@@ -7,51 +7,44 @@
         <div class="w-1/12 h-1 border-t-2 border-gray-700"></div>
       </div>
 
-      <!--Tabs navigation-->
-      <ul class="flex flex-row flex-wrap mx-auto mb-5 list-none border-b-0 ps-0" role="tablist" data-twe-nav-ref>
-        <li role="presentation">
-          <a href="#tabs-home"
-            class="my-2 block border-x-0 border-b-2 border-t-0 border-transparent px-7 pb-3.5 pt-4 text-xs font-medium uppercase leading-tight text-neutral-500 hover:isolate hover:border-transparent hover:bg-neutral-100 focus:isolate focus:border-transparent data-[twe-nav-active]:border-primary data-[twe-nav-active]:text-primary dark:text-white/50 dark:hover:bg-neutral-700/60 dark:data-[twe-nav-active]:text-primary"
-            data-twe-toggle="pill" data-twe-target="#tabs-home" data-twe-nav-active role="tab" aria-controls="tabs-home"
-            aria-selected="true">Home</a>
-        </li>
-        <li role="presentation">
-          <a href="#tabs-profile"
-            class="my-2 block border-x-0 border-b-2 border-t-0 border-transparent px-7 pb-3.5 pt-4 text-xs font-medium uppercase leading-tight text-neutral-500 hover:isolate hover:border-transparent hover:bg-neutral-100 focus:isolate focus:border-transparent data-[twe-nav-active]:border-primary data-[twe-nav-active]:text-primary dark:text-white/50 dark:hover:bg-neutral-700/60 dark:data-[twe-nav-active]:text-primary"
-            data-twe-toggle="pill" data-twe-target="#tabs-profile" role="tab" aria-controls="tabs-profile"
-            aria-selected="false">Profile</a>
-        </li>
-        <li role="presentation">
-          <a href="#tabs-messages"
-            class="my-2 block border-x-0 border-b-2 border-t-0 border-transparent px-7 pb-3.5 pt-4 text-xs font-medium uppercase leading-tight text-neutral-500 hover:isolate hover:border-transparent hover:bg-neutral-100 focus:isolate focus:border-transparent data-[twe-nav-active]:border-primary data-[twe-nav-active]:text-primary dark:text-white/50 dark:hover:bg-neutral-700/60 dark:data-[twe-nav-active]:text-primary"
-            data-twe-toggle="pill" data-twe-target="#tabs-messages" role="tab" aria-controls="tabs-messages"
-            aria-selected="false">Messages</a>
-        </li>
-        <li role="presentation">
-          <a href="#tabs-contact"
-            class="disabled pointer-events-none my-2 block border-x-0 border-b-2 border-t-0 border-transparent bg-transparent px-7 pb-3.5 pt-4 text-xs font-medium uppercase leading-tight text-neutral-400 hover:isolate hover:border-transparent hover:bg-neutral-100 focus:isolate focus:border-transparent dark:text-neutral-600"
-            data-twe-toggle="pill" data-twe-target="#tabs-contact" role="tab" aria-controls="tabs-contact"
-            aria-selected="false">Contact</a>
-        </li>
-      </ul>
+      <!-- Tabs navigation -->
+      <div class="flex justify-center">
+        <ul class="flex flex-row flex-wrap mx-auto mb-5 list-none border-b-0 ps-0 space-s-4" role="tablist"
+          data-twe-nav-ref>
+          <li role="presentation" class="mx-1">
+            <a href="#tabs-home"
+              class="my-2 block rounded-full border-2 border-gray-300 px-7 pb-3.5 pt-4 text-sm leading-tight text-neutral-600 font-semibold hover:isolate hover:border-gray-600 hover:bg-neutral-100 focus:outline-none focus:border-gray-800 data-[twe-nav-active]:border-gray-800 data-[twe-nav-active]:text-gray-800 capitalize"
+              data-twe-toggle="pill" data-twe-target="#tabs-home" data-twe-nav-active role="tab"
+              aria-controls="tabs-home" aria-selected="true">Home</a>
+          </li>
+          <li role="presentation" class="mx-1">
+            <a href="#tabs-profile"
+              class="my-2 block rounded-full border-2 border-gray-300 px-7 pb-3.5 pt-4 text-sm leading-tight text-neutral-600 font-semibold hover:isolate hover:border-gray-600 hover:bg-neutral-100 focus:outline-none focus:border-gray-800 data-[twe-nav-active]:border-gray-800 data-[twe-nav-active]:text-gray-800 capitalize"
+              data-twe-toggle="pill" data-twe-target="#tabs-profile" role="tab" aria-controls="tabs-profile"
+              aria-selected="false">Profile</a>
+          </li>
+          <li role="presentation" class="mx-1">
+            <a href="#tabs-messages"
+              class="my-2 block rounded-full border-2 border-gray-300 px-7 pb-3.5 pt-4 text-sm leading-tight text-neutral-600 font-semibold hover:isolate hover:border-gray-600 hover:bg-neutral-100 focus:outline-none focus:border-gray-800 data-[twe-nav-active]:border-gray-800 data-[twe-nav-active]:text-gray-800 capitalize"
+              data-twe-toggle="pill" data-twe-target="#tabs-messages" role="tab" aria-controls="tabs-messages"
+              aria-selected="false">Messages</a>
+          </li>
+        </ul>
+      </div>
 
-      <!--Tabs content-->
+      <!-- Tabs content -->
       <div class="mb-6">
         <div class="hidden opacity-100 transition-opacity duration-150 ease-linear data-[twe-tab-active]:block"
           id="tabs-home" role="tabpanel" aria-labelledby="tabs-home-tab" data-twe-tab-active>
-          Tab 1 content
+          Home tab content
         </div>
         <div class="hidden opacity-0 transition-opacity duration-150 ease-linear data-[twe-tab-active]:block"
           id="tabs-profile" role="tabpanel" aria-labelledby="tabs-profile-tab">
-          Tab 2 content
+          Profile tab content
         </div>
         <div class="hidden opacity-0 transition-opacity duration-150 ease-linear data-[twe-tab-active]:block"
           id="tabs-messages" role="tabpanel" aria-labelledby="tabs-profile-tab">
-          Tab 3 content
-        </div>
-        <div class="hidden opacity-0 transition-opacity duration-150 ease-linear data-[twe-tab-active]:block"
-          id="tabs-contact" role="tabpanel" aria-labelledby="tabs-contact-tab">
-          Tab 4 content
+          Messages tab content
         </div>
       </div>
     </div>
