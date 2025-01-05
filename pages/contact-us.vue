76711828991f37b410e28dd2d41f29d3ab32c6d4
AMRWERY <amrmounir2@gmail.com>
2025-01-05 17:55:28 +0200

    .

--- pages/contact-us.vue
+++ pages/contact-us.vue
@@ -3,12 +3,12 @@
         <section class="py-6 dark:bg-gray-800 dark:text-gray-50">
             <div class="grid max-w-6xl grid-cols-1 px-6 mx-auto lg:px-8 md:grid-cols-2 md:divide-x">
                 <div class="py-6 md:py-0 md:px-6">
-                    <h1 class="text-4xl font-bold">Get in touch</h1>
-                    <p class="pt-2 pb-4">Fill in the form to start a conversation</p>
+                    <h1 class="text-4xl font-bold">{{ $t('contact_us.get_in_touch') }}</h1>
+                    <p class="pt-2 pb-4">{{ $t('contact_us.fill_in_the_form_to_start_a_conversation') }}</p>
                     <div class="space-y-4">
                         <p class="flex items-center">
                             <icon name="material-symbols:location-on" class="w-5 h-5 me-2 sm:me-6" />
-                            <span>Damietta</span>
+                            <span>{{ $t('footer.damietta') }}</span>
                         </p>
                         <p class="flex items-center">
                             <icon name="material-symbols:call" class="w-5 h-5 me-2 sm:me-6" />
@@ -32,14 +32,13 @@
                             type="textarea" :validation="'required|length:10,500'" :required="true"
                             v-model="data.message" />
 
-
                         <div class="mt-6">
                             <button type="submit" class="w-[400px] px-4 py-2 btn-style" @click="sendMessage">
                                 <div class="flex items-center justify-center" v-if="loading">
                                     <span class="text-center me-2">{{ $t('loading_btn.logging') }}...</span>
                                     <icon name="svg-spinners:270-ring-with-bg" />
                                 </div>
-                                <span v-else>Send Your Message</span>
+                                <span v-else>{{ $t('btn.send_your_message') }}</span>
                             </button>
                         </div>
                     </ClientOnly>
