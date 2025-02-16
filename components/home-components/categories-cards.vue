dc1fa64476936a28b538c69548559556911bec24
AMRWERY <amrmounir2@gmail.com>
2025-02-16 18:07:36 +0200

    ....

--- components/home-components/categories-cards.vue
+++ components/home-components/categories-cards.vue
@@ -4,17 +4,18 @@
             <!-- skeleton-loader component -->
             <skeleton-loader />
         </div>
-        
+
         <div class="mx-auto my-12 max-w-7xl">
-            <div class="flex space-x-4">
-                <nuxt-link class="relative w-1/5 cursor-pointer group transit" v-for="card in store.categories"
-                    :key="card" :to="{ path: '/all-products', query: { categoryId: card.id } }">
-                    <div class="relative w-full h-48 overflow-hidden rounded-lg group-hover:opacity-75 sm:h-48">
-                        <img :src="card?.imgOne" class="object-cover object-center w-full h-full" />
+            <!-- Grid container: 1 column by default, 3 on medium screens, 5 on large screens -->
+            <div class="grid grid-cols-1 gap-4 md:grid-cols-3 lg:grid-cols-5">
+                <nuxt-link v-for="card in store.categories" :key="card.id"
+                    :to="{ path: '/all-products', query: { categoryId: card.id } }" class="relative group transit">
+                    <div class="relative h-48 overflow-hidden rounded-lg group-hover:opacity-75">
+                        <img :src="card?.imgOne" alt="category image"
+                            class="object-cover object-center w-full h-full" />
                         <div class="absolute inset-0 flex items-center justify-center">
                             <h3 class="relative overflow-hidden text-xl font-semibold text-white capitalize">
-                                {{ $i18n.locale === 'ar' ? card?.titleAr :
-                                    card?.title }}
+                                {{ $i18n.locale === 'ar' ? card?.titleAr : card?.title }}
                                 <span
                                     class="absolute bottom-0 start-0 w-0 h-0.5 bg-white transition-all duration-300 group-hover:w-full"></span>
                             </h3>
