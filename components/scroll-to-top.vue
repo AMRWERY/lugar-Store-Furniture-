47e132a782dfa99ed0060946c2b6c301c3450bc8
AMRWERY <amrmounir2@gmail.com>
2025-02-21 17:23:27 +0200

    handle whatsapp-componenet and scroll-to-top

--- components/scroll-to-top.vue
+++ components/scroll-to-top.vue
@@ -1,7 +1,7 @@
 <template>
     <div>
         <button v-if="isVisible" @click="scrollToTop"
-            class="fixed flex items-center justify-center p-3 text-black transition-all duration-300 bg-gray-300 border rounded-full shadow-lg bottom-36 end-8"
+            class="fixed z-50 flex items-center justify-center px-4 py-4 border rounded-full shadow-lg btn-style bottom-20 end-8"
             aria-label="Scroll to top">
             <i class="fa-solid fa-arrow-up"></i>
         </button>
--- components/whatsapp-componenet.vue
+++ components/whatsapp-componenet.vue
@@ -1,9 +1,8 @@
 <template>
     <div>
-        <!-- Button to open the dialog -->
         <nuxt-link to="https://wa.me/201023351915" target="_blank" role="button"
-            class="fixed z-50 flex items-center justify-center p-3 bg-[#d1d5db] text-white transition-all duration-300 rounded-full shadow-lg bottom-52 end-8 focus:ring-4 active:scale-90">
-            <i class="fa-brands fa-whatsapp text-2xl text-[#25D366]"></i>
+            class="fixed z-50 flex items-center justify-center py-2 px-3.5 bg-[#25D366] text-white transition-all duration-300 rounded-full shadow-lg bottom-36 end-8 focus:ring-4 active:scale-90">
+            <i class="text-2xl fa-brands fa-whatsapp"></i>
         </nuxt-link>
     </div>
 </template>
