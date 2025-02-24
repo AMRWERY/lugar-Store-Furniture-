a2a68c30bb9bc99030f5cb7f549202660c535790
AMRWERY <amrmounir2@gmail.com>
2025-02-24 16:48:10 +0200

    handle product details design

--- pages/all-products/[id].vue
+++ pages/all-products/[id].vue
@@ -3,70 +3,66 @@
     <!--breadcrumb component -->
     <breadcrumb />
 
-    <div class="py-6 bg-white sm:py-8 lg:py-12">
-      <div class="max-w-screen-lg px-4 mx-auto">
-        <div class="grid gap-8 md:grid-cols-2">
-          <div class="space-y-4">
-            <div class="relative overflow-hidden bg-gray-100 rounded-lg aspect-square h-96">
-              <img :src="selectedImage" loading="lazy" class="object-cover object-center w-full h-full" />
-              <span class="absolute start-0 top-0 rounded-br-lg bg-red-500 px-3 py-1.5 text-sm tracking-wider text-white"
-                v-if="Number(selectedProduct?.discount) > 0">{{
-                  selectedProduct?.discount }}% {{ $t('products.off') }}</span>
+    <div class="font-[sans-serif] p-4">
+      <div class="max-w-xl mx-auto xl:max-w-screen-xl lg:max-w-screen-lg">
+        <div class="grid items-start grid-cols-1 gap-8 lg:grid-cols-5 max-lg:gap-12 max-sm:gap-8">
+          <div class="top-0 w-full lg:sticky lg:col-span-3">
+            <div class="flex flex-row gap-2">
+              <div class="flex flex-col w-16 gap-2 space-y-4 max-sm:w-10 shrink-0">
+                <img v-for="(image, index) in imageList" :key="index" @click="setSelectedImage(image)" :src="image"
+                  alt="Product1"
+                  class="aspect-[64/85] object-cover object-top w-full cursor-pointer border-b-2 border-black" />
+              </div>
+              <div class="relative flex-1">
+                <img :src="selectedImage" alt="Product" class="w-full aspect-[750/800] object-top object-cover" />
+                <span
+                  class="absolute start-0 top-0 rounded-br-lg bg-red-500 px-3 py-1.5 text-sm tracking-wider text-white"
+                  v-if="Number(selectedProduct?.discount) > 0">{{
+                    selectedProduct?.discount }}% {{ $t('products.off') }}</span>
+              </div>
             </div>
-            <ClientOnly>
-              <Carousel v-bind="config">
-                <Slide v-for="(image, index) in imageList" :key="index">
-                  <div class="carousel__item">
-                    <button @click="setSelectedImage(image)" type="button" class="block">
-                      <img :src="image" class="object-cover w-full h-32" />
-                    </button>
-                  </div>
-                </Slide>
-                <template #addons>
-                  <Navigation />
-                </template>
-              </Carousel>
-            </ClientOnly>
           </div>
 
-          <div class="md:py-8">
-            <div class="mb-2 md:mb-3">
-              <span class="mb-0.5 inline-block text-gray-500">{{ selectedProduct?.categoryTitle }}</span>
-              <h2 class="text-2xl font-bold text-gray-800 lg:text-3xl">{{ $i18n.locale === 'ar' ?
+          <div class="w-full lg:col-span-2">
+            <div>
+              <h3 class="text-lg font-bold text-gray-800">{{ $i18n.locale === 'ar' ?
                 selectedProduct?.titleAr
                 :
-                selectedProduct?.title }}</h2>
-            </div>
-
-            <div class="mb-4">
-              <div class="flex items-end gap-2">
-                <span class="text-xl font-bold text-gray-800 md:text-2xl">{{ selectedProduct?.discountedPrice }}
-                  {{ $t('products.le') }}</span>
-                <span class="mb-0.5 text-red-500 line-through" v-if="selectedProduct?.originalPrice">{{
-                  selectedProduct?.originalPrice }} {{ $t('products.le') }}</span>
+                selectedProduct?.title }}</h3>
+              <div class="flex items-center mt-2 space-s-1">
+                <p class="text-sm text-gray-800 !ml-3">{{ selectedProduct?.categoryTitle }}</p>
+              </div>
+              <div class="flex flex-wrap items-center gap-4 mt-6">
+                <h4 class="text-2xl font-bold text-gray-800">{{ selectedProduct?.discountedPrice }}
+                  {{ $t('products.le') }}</h4>
+                <p class="text-lg text-gray-500 line-through">{{
+                  selectedProduct?.originalPrice }} {{ $t('products.le') }}</p>
               </div>
             </div>
 
-            <div class="my-8 me-4">
-              <div class="mb-3 text-lg font-semibold text-gray-800">{{ $t('products.quantity') }}</div>
-              <div class="w-32">
-                <div class="relative flex flex-row w-full h-10 bg-transparent rounded-lg">
-                  <button @click="decrementQuantity"
-                    class="w-32 h-full text-gray-600 bg-gray-100 border-r outline-none cursor-pointer rounded-s hover:text-gray-700 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
-                    <i class="m-auto fa-solid fa-minus"></i>
+            <hr class="my-6 border-gray-300" />
+
+            <div>
+              <div>
+                <h3 class="text-xl font-bold text-gray-800">{{ $t('products.quantity') }}</h3>
+                <div class="flex mt-4 overflow-hidden border divide-x rounded w-max">
+                  <button type="button" class="w-12 h-10 font-semibold bg-gray-100" @click="decrementQuantity">
+                    <i class="inline m-auto fa-solid fa-minus"></i>
                   </button>
                   <input type="number" v-model="quantity"
-                    class="flex items-center w-[70px] font-semibold text-center text-gray-700 placeholder-gray-700 bg-gray-100 outline-none focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2 text-md hover:text-black"
+                    class="flex items-center justify-center w-12 h-10 text-lg font-semibold text-gray-800 bg-transparent"
                     placeholder="1" />
-                  <button @click="incrementQuantity"
-                    class="w-32 h-full text-gray-600 bg-gray-100 border-r outline-none cursor-pointer rounded-e hover:text-gray-700 hover:bg-gray-300 focus:outline-none focus:ring-2 focus:ring-blue-500 focus:ring-offset-2">
-                    <i class="m-auto fa-solid fa-plus"></i>
+                  <button type="button" class="w-12 h-10 font-semibold text-white bg-gray-800"
+                    @click="incrementQuantity">
+                    <i class="inline m-auto fa-solid fa-plus"></i>
                   </button>
                 </div>
               </div>
             </div>
 
-            <div class="flex gap-2.5">
+            <hr class="my-6 border-gray-300" />
+
+            <div class="flex flex-wrap gap-4 mt-6">
               <button @click="handleAddToCart" class="w-[300px] h-10 btn-style flex items-center justify-center">
                 <div class="flex items-center justify-center" v-if="loading">
                   <span class="text-center me-2">{{ $t('loading_btn.adding_to_cart') }}...</span>
@@ -78,52 +74,22 @@
                 </div>
               </button>
             </div>
-
-            <div class="mt-10 md:mt-16 lg:mt-20">
-              <div class="mb-3 text-lg font-semibold text-gray-800">{{ $t('products.description') }}</div>
-              <p class="text-gray-500">
-                {{ $i18n.locale === 'ar' ? selectedProduct?.descriptionAr : selectedProduct?.description }}
-              </p>
-            </div>
           </div>
         </div>
       </div>
+    </div>
 
-      <!-- dynamic-toast component  -->
-      <div class="fixed z-50 pointer-events-none bottom-5 start-5 w-96">
-        <div class="pointer-events-auto">
-          <dynamic-toast v-if="showToast" :title="toastTitle" :message="toastMessage" :toastType="toastType"
-            :duration="5000" :toastIcon="toastIcon" @toastClosed="showToast = false" />
-        </div>
+    <!-- dynamic-toast component -->
+    <div class="fixed z-50 pointer-events-none bottom-5 start-5 w-96">
+      <div class="pointer-events-auto">
+        <dynamic-toast v-if="showToast" :title="toastTitle" :message="toastMessage" :toastType="toastType"
+          :duration="5000" :toastIcon="toastIcon" @toastClosed="showToast = false" />
       </div>
     </div>
   </div>
 </template>
 
 <script setup>
-const config = {
-  itemsToShow: 3,
-  wrapAround: true,
-  breakpoints: {
-    200: {
-      itemsToShow: 1.5,
-      snapAlign: 'center',
-    },
-    400: {
-      itemsToShow: 2,
-      snapAlign: 'start',
-    },
-    700: {
-      itemsToShow: 3.5,
-      snapAlign: 'center',
-    },
-    1024: {
-      itemsToShow: 3,
-      snapAlign: 'start',
-    },
-  },
-};
-
 const store = useProductsStore()
 const route = useRoute()
 const selectedImage = ref('');
